4f3991072e982b6f3db570b0e8fc48afdc026874
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2025-11-10 00:38:33 +0100

    refactor: Hero component enhanced

--- src/app/shared/components/hero/hero.ts
+++ src/app/shared/components/hero/hero.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DarkModeService } from '@shared/services/darkmode/darkmode';
+import { ZardButtonComponent } from '../ui/button/button.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [ZardButtonComponent],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {}
+export class Hero {
+  private readonly darkmodeService = inject(DarkModeService);
+
+  getCurrentTheme(): 'light' | 'dark' {
+    return this.darkmodeService.getCurrentTheme();
+  }
+}
