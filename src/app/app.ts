7fca76c854f41dac9615c705282491d9e64ef410
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2025-11-09 18:12:24 +0100

    feat: theme toggle added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DarkModeService } from '@shared/services/darkmode/darkmode';
 import { Footer } from './shared/components/footer/footer';
 import { Header } from './shared/components/header/header';
 import { PageHeader } from './shared/components/page-header/page-header';
@@ -9,6 +10,11 @@ import { PageHeader } from './shared/components/page-header/page-header';
   imports: [RouterOutlet, Header, Footer, PageHeader],
   templateUrl: './app.html',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('car-shop');
+  private readonly darkmodeService = inject(DarkModeService);
+
+  ngOnInit(): void {
+    this.darkmodeService.initTheme();
+  }
 }
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,14 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { Route, ROUTES } from '@angular/router';
+import { DarkModeService } from '@shared/services/darkmode/darkmode';
+import { ZardButtonComponent } from '../ui/button/button.component';
+import { ZardIconComponent } from '../ui/icon/icon.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ZardButtonComponent, ZardIconComponent],
   templateUrl: './header.html',
 })
 export class Header {
+  private readonly darkmodeService = inject(DarkModeService);
+
   public menuItems: unknown[];
   public authItems: unknown[];
+
   constructor() {
     const routes = inject(ROUTES)[0] as Route[];
     this.menuItems = routes
@@ -28,5 +34,12 @@ export class Header {
         routerLink: [route.path],
         routerLinkActiveOptions: { exact: true },
       }));
+  }
+  toggleTheme(): void {
+    this.darkmodeService.toggleTheme();
+  }
+
+  getCurrentTheme(): 'light' | 'dark' {
+    return this.darkmodeService.getCurrentTheme();
   }
 }
