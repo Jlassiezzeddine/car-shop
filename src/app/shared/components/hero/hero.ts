371d322c79d5548a7c31a0bdd0b63f244dbd7a0a
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2025-11-10 01:05:44 +0100

    feature: scroll behavior smooth + scroll to element

--- src/app/shared/components/hero/hero.ts
+++ src/app/shared/components/hero/hero.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { DarkModeService } from '@shared/services/darkmode/darkmode';
 import { ZardButtonComponent } from '../ui/button/button.component';
@@ -10,8 +11,15 @@ import { ZardButtonComponent } from '../ui/button/button.component';
 })
 export class Hero {
   private readonly darkmodeService = inject(DarkModeService);
+  private readonly viewportScroller = inject(ViewportScroller);
 
   getCurrentTheme(): 'light' | 'dark' {
     return this.darkmodeService.getCurrentTheme();
+  }
+
+  scrollToElement(elementId: string): void {
+    // Set a top offset of 90 pixels
+    this.viewportScroller.setOffset([0, 90]);
+    this.viewportScroller.scrollToAnchor(elementId);
   }
 }
