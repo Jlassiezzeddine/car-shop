3c71282477004fabae41c777b80500deb0313e88
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2025-11-10 01:06:08 +0100

    refactor: footer enhanced

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,30 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { Route, RouterLink, ROUTES } from '@angular/router';
+import { ZardDividerComponent } from '../ui/divider/divider.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, ZardDividerComponent],
   templateUrl: './footer.html',
 })
 export class Footer {
-  protected menuItems: unknown[];
+  protected menuItems: Route[];
   protected currentYear: number = new Date().getFullYear();
 
   constructor() {
     const routes = inject(ROUTES)[0] as Route[];
-    this.menuItems = routes
-      .filter(
-        (route) =>
-          route.path &&
-          !route.path.includes('auth') &&
-          !route.path.includes('home') &&
-          route.data?.['label'],
-      )
-      .map((route) => ({
-        label: route.data?.['label'],
-        icon: route.data?.['icon'] || '',
-        routerLink: [route.path],
-        routerLinkActiveOptions: { exact: true },
-      }));
+    this.menuItems = routes.filter((route) => !route.path?.includes('auth') && route.title);
   }
 }
